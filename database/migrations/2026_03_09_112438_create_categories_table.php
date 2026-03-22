7504aeaf6c02ef694c8b7034b2a1029e59a3f02f
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-03-22 15:11:33 +0100

    modification on the category migration to add missing fields to the table

--- database/migrations/2026_03_09_112438_create_categories_table.php
+++ database/migrations/2026_03_09_112438_create_categories_table.php
@@ -9,9 +9,10 @@ return new class extends Migration {
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->unique();
+            $table->text('description')->nullable();
             $table->string('color', 7)->default('#000000');
             $table->boolean('is_active')->default(true);
-            $table->string('name')->unique();
             $table->timestamps();
         });
     }
