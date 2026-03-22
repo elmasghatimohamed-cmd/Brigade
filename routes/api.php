1a6af275c1eab8c9b100ed3f79e2e146364fbfb8
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-03-22 18:04:49 +0100

    Adding ingredients endpoints with modification on categories and plats endpoints

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PlatController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\IngredientController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -17,19 +18,32 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/plats', [PlatController::class, 'store']);
-    Route::get('/plats', [PlatController::class, 'index']);
-    Route::get('/plats/{id}', [PlatController::class, 'show']);
-    Route::put('/plats/{id}', [PlatController::class, 'update']);
-    Route::delete('/plats/{id}', [PlatController::class, 'destroy']);
+    Route::get('/plates', [PlatController::class, 'index']);
+    Route::get('/plates/{id}', [PlatController::class, 'show']);
 });
 
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('/plates', [PlatController::class, 'store']);
+    Route::put('/plates/{id}', [PlatController::class, 'update']);
+    Route::delete('/plates/{id}', [PlatController::class, 'destroy']);
+});
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/categories', [CategoryController::class, 'store']);
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    Route::get('/categories/{id}/plates', [CategoryController::class, 'getPlats']);
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('/categories', [CategoryController::class, 'store']);
     Route::put('/categories/{id}', [CategoryController::class, 'update']);
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
     Route::post('/categories/{id}/plats', [CategoryController::class, 'addPlats']);
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::get('/ingredients', [IngredientController::class, 'index']);
+    Route::post('/ingredients', [IngredientController::class, 'store']);
+    Route::put('/ingredients/{id}', [IngredientController::class, 'update']);
+    Route::delete('/ingredients/{id}', [IngredientController::class, 'destroy']);
 });
