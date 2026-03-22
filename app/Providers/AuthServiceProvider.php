079aa226124606e5a9e213519d91c62e6801ab91
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-03-22 18:03:24 +0100

    Adding ingredient policies to the auth service provider in order to apply authorization rules

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,14 +6,17 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 use Illuminate\Support\Facades\Gate;
 use App\Models\Plat;
 use App\Models\Category;
+use App\Models\Ingredient;
 use App\Policies\PlatPolicy;
 use App\Policies\CategoryPolicy;
+use App\Policies\IngredientPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         Plat::class => PlatPolicy::class,
         Category::class => CategoryPolicy::class,
+        Ingredient::class => IngredientPolicy::class,
     ];
 
     public function register(): void
